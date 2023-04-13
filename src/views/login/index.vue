09b4ce8388a34947c14724c70f57ef9ff029a683
saber <1558365539@qq.com>
2023-04-13 15:26:50 +0800

    feat: 环境构建

---
+++ src/views/login/index.vue
@@ -0,0 +1,5 @@
+<template>login</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import { UserConfigExport, ConfigEnv, loadEnv } from "vite";
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+/** 当前执行node命令时文件夹的地址（工作目录） */
+const root: string = process.cwd();
+
+export default ({ command, mode }: ConfigEnv): UserConfigExport => {
+  const env = loadEnv(mode, root)
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY } = env
+  const isBuild = command === 'build'
+  console.log(env, isBuild)
+  return{
+    plugins: [vue()],
+    base: VITE_PUBLIC_PATH,
+    root,
+    resolve: {
+      alias: [
+        { find: '@', replacement: '/src' },
+      ],
+    },
+    server: {
+      https: false,
+      host: true,
+      port: VITE_PORT,
+      proxy: VITE_PROXY,
+    },
+  }
+}
